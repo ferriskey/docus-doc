3d925a03e7eb64ae437dbb54725fb0ffed8b0262
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2025-11-24 16:32:20 +0100

    feat: update docs config, project structure, and i18n

--- docs/app/app.config.ts
+++ docs/app/app.config.ts
@@ -1,34 +1,36 @@
 export default defineAppConfig({
   socials: {
-    x: 'https://x.com/nuxt_js',
-    discord: 'https://discord.com/invite/ps2h6QT',
-    nuxt: 'https://nuxt.com',
+    x: "https://x.com/nuxt_js",
+    discord: "https://discord.com/invite/ps2h6QT",
+    nuxt: "https://nuxt.com",
   },
   github: {
-    rootDir: 'docs',
+    rootDir: "docs",
   },
   toc: {
     bottom: {
-      links: [{
-        icon: 'i-lucide-book-open',
-        label: 'Nuxt UI docs',
-        to: 'https://ui.nuxt.com/getting-started/installation/nuxt',
-        target: '_blank',
-      }],
+      links: [
+        {
+          icon: "material-symbols:cloud",
+          label: "Cloud IAM",
+          to: "https://cloud-iam.com",
+          target: "_blank",
+        },
+      ],
     },
   },
   ui: {
     pageHero: {
       slots: {
-        title: 'font-semibold sm:text-6xl',
-        container: '!pb-0',
+        title: "font-semibold sm:text-6xl",
+        container: "!pb-0",
       },
     },
     pageCard: {
       slots: {
-        container: 'lg:flex min-w-0',
-        wrapper: 'flex-none',
+        container: "lg:flex min-w-0",
+        wrapper: "flex-none",
       },
     },
   },
-})
+});
