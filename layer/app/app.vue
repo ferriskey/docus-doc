a3d6aaf57b26a1d4fcaaa2e4e22afd5c681abfd9
Nathael Bonnal <pro.nathaelbonnal@gmail.com>
2025-11-24 16:56:37 +0100

    feat: update fivecon

--- layer/app/app.vue
+++ layer/app/app.vue
@@ -15,7 +15,7 @@ useHead({
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
     ],
     link: [
-        { rel: 'icon', href: '/favicon.ico' },
+        { rel: 'icon', href: '/icon.png' },
     ],
     htmlAttrs: {
         lang,
